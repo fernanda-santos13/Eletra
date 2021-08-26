a595c4e0c7e0072dbdc1bd88835682382097eb5a
Fernanda dos Santos <fs.fernandasantos553@gmail.com>
2021-08-26 10:36:14 -0300

    ###Concluído

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Banner />
+    <div class="main-container">
+      <h1>Monte o seu Burger:</h1>
+      <burguer-form />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Banner from '../components/Banner'
+import BurguerForm from '../components/BurguerForm'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Banner,
+    BurguerForm
   }
 }
 </script>
